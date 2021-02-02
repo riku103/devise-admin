e3b61cd3ff89cad3a6cfc41893204f3b6a9ffa96
新田陸人 <>
2021-02-02 16:21:52 +0900

    お知らせの新規投稿機能の実装

--- app/controllers/admin/notifications_controller.rb
+++ app/controllers/admin/notifications_controller.rb
@@ -3,8 +3,17 @@ class Admin::NotificationsController < Admin::AdminController
   end
 
   def new
+    @notification = Notification.new
   end
 
   def create
+    notification = Notification.create!(notification_params)
+    redirect_to admin_notification_path(notification)
+  end
+
+  private
+
+  def notification_params
+    params.require(:notification).permit(:title, :content)
   end
 end
